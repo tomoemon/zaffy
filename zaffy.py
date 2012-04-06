3b6aa4b46e172ac3afadf758df7b2e80d80f75cf
tomoemon <bach48+github_tomoemon@gmail.com>
2012-04-06 21:14:32 +0900

    マルチドキュメントが使えるとちょっと便利かもしれない？

--- zaffy.py
+++ zaffy.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
+import yaml
+import sys
 
+def load_yaml():
+  return yaml.load_all(file(sys.argv[1]))
+
+def main():
+  print len(list(load_yaml()))
+  for doc in load_yaml():
+    print doc
+
+if __name__ == '__main__':
+  main()
