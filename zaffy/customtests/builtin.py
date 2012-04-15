ec26479c53052817109a5f67cfdaa0bdeda42f1d
tomoemon <bach48+github_tomoemon@gmail.com>
2012-04-16 00:40:58 +0900

    ・assert時にfilterを使った場合に、値を後からチェックできるように関数をラップする

---
+++ zaffy/customtests/builtin.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+is_tocmp_on_asserting = [
+  ['callable', 1],
+  ['defined', 1],
+  ['divisibleby', 1],
+  ['escaped', 1],
+  ['even', 1],
+  ['iterable', 1],
+  ['lower', 1],
+  ['mapping', 1],
+  ['none', 1],
+  ['number', 1],
+  ['odd', 1],
+  ['sameas', 2],
+  ['sequence', 1],
+  ['string', 1],
+  ['undefined', 1],
+  ['upper', 1]
+]
+
