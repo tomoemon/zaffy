ceda44645764827343741c808357c4e5eb7ea542
tomoemon <bach48+github_tomoemon@gmail.com>
2012-12-10 23:47:09 +0900

    xmlパースを別途作成

--- zaffy/customfilters/xpath.py
+++ zaffy/customfilters/xpath.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-from lxml import etree
+import lxml
+import lxml.etree
 
 def do_xpathlist(value, xpath, namespaces=None):
   """ 常に list 形式で返す """
-  root = etree.fromstring(str(value))
+  if isinstance(value, lxml.etree._Element):
+    root = value
+  else:
+    root = lxml.etree.fromstring(str(value))
   result = root.xpath(xpath, namespaces=namespaces)
   return result
 
 def do_xpath(value, xpath, namespaces=None):
   """ 返り値が1件だけの場合は中身を取り出して返す """
-  root = etree.fromstring(str(value))
-  result = root.xpath(xpath, namespaces=namespaces)
+  result = do_xpathlist(value, xpath, namespaces)
   if len(result) == 1:
     return result[0]
   return result
