2202a32dc43ce192653688acaafca3fecafe2779
tomoemon <bach48+github_tomoemon@gmail.com>
2012-12-14 03:02:00 +0900

    iniファイル filter を追加

---
+++ zaffy/customfilters/asini.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+try:
+  import configparser
+except ImportError:
+  import ConfigParser as configparser
+
+import io
+
+def do_asini(value):
+  dummy_section = "__{0}__".format(id(value))
+  value = "[{0}]\n".format(dummy_section) + value
+  print value
+  config = configparser.SafeConfigParser(allow_no_value=True)
+  config.readfp(io.BytesIO(value))
+
+  if len(config.sections()) == 1:
+    result = {key: value for key, value in config.items(dummy_section)}
+  else:
+    config.remove_section(dummy_section)
+    result = {}
+    for section in config.sections():
+      result[section] = \
+          {key: value for key, value in config.items(section)}
+  return result
+
+if __name__ == '__main__':
+  d = """[hoge]
+fuga=piyo
+x= 10
+y=
+"""
+  print do_asini(d)
+
+  d = """
+fuga=piyo
+x= 10
+y=
+"""
+  print do_asini(d)
