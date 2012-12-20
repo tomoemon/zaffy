76e6224530e46759f014f0c62efa8eb58c7d8fb6
deflis <deflis@gmail.com>
2012-12-20 22:12:39 +0900

    リクエストのQueryStringを自動でパースしてくれるように修正

--- zaffy/customfilters/asurl.py
+++ zaffy/customfilters/asurl.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
-from urlparse import urlparse
+from urlparse import urlparse, parse_qs, ParseResult
 
 def do_asurl(value):
-    return urlparse(str(value))
+  parsed = urlparse(str(value))
+  response = ParseResultWithQuery(parsed.scheme, parsed.netloc, parsed.path, parsed.params, parsed.query, parsed.fragment)
+  return response
+
+class ParseResultWithQuery(ParseResult):
+  @property
+  def queries(self):
+    return parse_qs(self.query)
