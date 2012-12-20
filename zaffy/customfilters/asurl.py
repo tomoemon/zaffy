482f1b9861d29c64ceb78a8ecf018938c30a0fbc
tomoemon <bach48+github_tomoemon@gmail.com>
2012-12-21 02:00:13 +0900

    urlparseのテストコード追加。パラメータ名等の変更

--- zaffy/customfilters/asurl.py
+++ zaffy/customfilters/asurl.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
-from urlparse import urlparse, parse_qs, ParseResult
+from urlparse import urlsplit, parse_qs, parse_qsl, SplitResult
 
 def do_asurl(value):
-  parsed = urlparse(str(value))
-  response = ParseResultWithQuery(parsed.scheme, parsed.netloc, parsed.path, parsed.params, parsed.query, parsed.fragment)
+  parsed = urlsplit(unicode(value))
+  response = SplitResultWithQuery(parsed.scheme, parsed.netloc,
+      parsed.path, parsed.query, parsed.fragment)
   return response
 
-class ParseResultWithQuery(ParseResult):
+class SplitResultWithQuery(SplitResult):
   @property
-  def queries(self):
-    return parse_qs(self.query)
+  def param(self):
+    return parse_qs(self.query, True)
+
+  @property
+  def paramlist(self):
+    return parse_qsl(self.query, True)
+
+  @property
+  def hash(self):
+    return self.fragment
+
+def do_asurlquery(value):
+  return parse_qs(unicode(value), True)
+
+def do_asurlquerylist(value):
+  return parse_qsl(unicode(value), True)
+
