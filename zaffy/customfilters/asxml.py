6f7bc3c6e78d2b5a0ee3682cf95bca242593b6d9
tomoemon <bach48+github_tomoemon@gmail.com>
2013-01-13 20:01:16 +0900

    フィルターの名前を変更（importがバッティングする場合のみ _ 付きのファイル名にする）

--- zaffy/customfilters/asxml.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from lxml import etree
-
-
-def do_asxml(value):
-  return etree.fromstring(str(value))
-
---
+++ zaffy/customfilters/xml.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import lxml
+from lxml import etree
+
+
+def do_asxml(value):
+  return etree.fromstring(str(value))
+
+
+def do_xpathlist(value, xpath, namespaces=None):
+  """ 常に list 形式で返す """
+  if getattr(value, 'xpath', None):
+    root = value
+  else:
+    root = lxml.etree.fromstring(str(value))
+  result = root.xpath(xpath, namespaces=namespaces)
+  return result
+
+
+def do_xpath(value, xpath, namespaces=None):
+  """ 返り値が1件だけの場合は中身を取り出して返す """
+  result = do_xpathlist(value, xpath, namespaces)
+  if len(result) == 1:
+    return result[0]
+  return result
