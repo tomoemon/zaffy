619117f6560088a6cff9153284a961b610043c51
tomoemon <bach48+github_tomoemon@gmail.com>
2013-02-20 03:51:55 +0900

    file アクションのメソッド名変更 remove -> delete

--- zaffy/actions/file.py
+++ zaffy/actions/file.py
@@ -7,46 +7,56 @@ from baseaction import BaseAction
 
 class File(BaseAction):
   """ File アクション
-  ファイルに関する検査などを行なう
+
+  ファイル操作やファイルに関する情報の取得を行なう
   """
   @classmethod
   def exists(cls, filepath):
+    """ exists """
     return os.access(filepath, os.F_OK)
 
   @classmethod
   def writable(cls, filepath):
+    """ writable """
     return os.access(filepath, os.W_OK)
 
   @classmethod
   def readable(cls, filepath):
+    """ readable """
     return os.access(filepath, os.R_OK)
 
   @classmethod
   def executable(cls, filepath):
+    """ executable """
     return os.access(filepath, os.R_OK)
 
   @classmethod
   def size(cls, filepath):
+    """ size """
     stat_value = os.stat(filepath)
     return stat_value.st_size
 
   @classmethod
   def access_time(cls, filepath):
+    """ access_time """
     stat_value = os.stat(filepath)
     return datetime.fromtimestamp(stat_value.st_atime)
 
   @classmethod
   def update_time(cls, filepath):
+    """ update_time """
     stat_value = os.stat(filepath)
     return datetime.fromtimestamp(stat_value.st_mtime)
 
   @classmethod
   def create_time(cls, filepath):
+    """ create_time """
     stat_value = os.stat(filepath)
     return datetime.fromtimestamp(stat_value.st_ctime)
 
   @classmethod
   def read(cls, filepath, size=-1, offset=0, encoding=None):
+    """ read """
     import codecs
     if encoding:
       with codecs.open(filepath, encoding=encoding) as fp:
@@ -58,18 +68,22 @@ class File(BaseAction):
         return fp.read(size)
 
   def do_copy(self, path, to):
+    """ copy """
     shutil.copy(path, to)
 
-  def do_remove(self, path):
+  def do_delete(self, path):
+    """ remove """
     if os.path.isdir(path):
       shutil.rmtree(path)
     else:
       os.unlink(path)
 
   def do_rename(self, path, to):
+    """ rename """
     shutil.move(path, to)
 
   def do_write(self, path, data, mode="wb"):
+    """ write """
     with open(path, mode) as fp:
       fp.write(data)
 
