28bb4d2c6e25d7d06217ff1760984dfd5e6abe8c
tomoemon <bach48+github_tomoemon@gmail.com>
2013-03-19 15:59:01 +0900

    add document

--- lib/actions/local.py
+++ lib/actions/local.py
@@ -6,13 +6,33 @@ class Local(BaseAction):
   """ Local アクション
 
   シナリオ単位のローカル変数を保存する
+
+  上書き可能なローカル変数をシナリオ内で定義する。:ref:`references-actions-const-label` と異なるのは上書き可能な点と、シナリオ内でのみ有効な点。シナリオ A が、シナリオ B (local アクションを実行する) を :ref:`references-actions-require-label` しても、シナリオ A から B のローカル変数を参照することはできない (:ref:`references-actions-const-label` なら参照可能)
   """
   def do_local(self, scenario, **kwargs):
     """ do_set の省略呼び出し """
     self.do_set(scenario, **kwargs)
 
   def do_set(self, scenario, **kwargs):
-    """ set """
+    """ ローカル変数の定義
+
+    .. code-block:: yaml
+
+       - サンプルシナリオ
+
+       - action: local
+         user: taro
+         pass: taropass
+
+       - action: http.get
+         url: http://yahoo.co.jp/
+         params:
+           user: <<local.user>>
+           pass: <<local.pass>>
+
+    :param any (any): 任意のキーで任意の値を指定する
+    :return dict: 現在定義されているローカル変数の辞書を返す
+  """
     for key, value in kwargs.items():
       scenario.localvar[key] = value
 
