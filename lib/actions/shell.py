ab44dfb0efc0f2fd72d1a6544a6c027101e1ccfa
tomoemon <bach48+github_tomoemon@gmail.com>
2013-05-27 20:51:11 +0900

    add 'error_on_failed' option to shell action

--- lib/actions/shell.py
+++ lib/actions/shell.py
@@ -11,11 +11,11 @@ class Shell(BaseAction):
 
   シェルを通してプロセスの実行などを行なう
   """
-  def do_shell(self, cmd, stdin=None, curdir=None, shell=True):
+  def do_shell(self, cmd, stdin=None, curdir=None, shell=True, error_on_failed=False):
     """ do_run の省略呼び出し """
-    self.do_run(cmd, stdin, curdir, shell)
+    self.do_run(cmd, stdin, curdir, shell, error_on_failed)
 
-  def do_run(self, cmd, stdin=None, curdir=None, shell=True):
+  def do_run(self, cmd, stdin=None, curdir=None, shell=True, error_on_failed=False):
     """ コマンドの実行
 
     .. code-block:: yaml
@@ -30,7 +30,8 @@ class Shell(BaseAction):
 
     :param string cmd: 実行するコマンド
     :param string curdir: 実行時のカレントディレクトリ。指定しない場合は zaffy 実行時のカレントディレクトリ
-    :param bool shell: True: シェルを介して実行する
+    :param bool shell: シェルを介して実行する (default:True)
+    :param bool error_on_failed: コマンドの失敗時(return code != 0) にテストを error 扱いにする (default:False)
     :return: - **stdout** (*string*) - 実行したコマンドの標準出力
              - **stderr** (*string*) - 実行したコマンドの標準エラー
              - **returncode** (*int*) - 実行したコマンドの終了ステータス
@@ -49,6 +50,9 @@ class Shell(BaseAction):
 
     stdoutdata = util.unicode_os_string(stdoutdata)
     stderrdata = util.unicode_os_string(stderrdata)
+
+    if error_on_failed and proc.returncode != 0:
+      raise Exception(stderrdata.strip() + ' (code=' + util.unicode(proc.returncode) + ')')
 
     self.result = {
         'stdout': stdoutdata,
