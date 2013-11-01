81da4f8e4c00adf62e6d4a6284f15005f62fb55d
tomoemon <bach48+github_tomoemon@gmail.com>
2013-11-01 21:52:45 +0900

    use yaml.dump when yaml.safe_dump raises RepresenterError (eg. dump some kind of object)

--- lib/formatter/YamlDumper.py
+++ lib/formatter/YamlDumper.py
@@ -19,10 +19,25 @@ def literal_unicode_representer(dumper, data):
   return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
 
 
+def tuple_representer(dumper, data):
+  return dumper.represent_sequence(u'tag:yaml.org,2002:seq', data)
+
+
+def unicode_representer(dumper, data):
+  return dumper.represent_scalar(u'tag:yaml.org,2002:str', data)
+
+
 yaml.SafeDumper.add_representer(folded_unicode, folded_unicode_representer)
 yaml.SafeDumper.add_representer(literal_unicode, literal_unicode_representer)
+yaml.Dumper.add_representer(folded_unicode, folded_unicode_representer)
+yaml.Dumper.add_representer(literal_unicode, literal_unicode_representer)
+yaml.Dumper.add_representer(tuple, tuple_representer)
+yaml.Dumper.add_representer(util._unicode, unicode_representer)
 
 
 def dump(obj):
-  return yaml.safe_dump(obj, default_flow_style=False)
+  try:
+    return yaml.safe_dump(obj, default_flow_style=False, allow_unicode=True)
+  except yaml.representer.RepresenterError as e:
+    return yaml.dump(obj, default_flow_style=False, allow_unicode=True)
 
